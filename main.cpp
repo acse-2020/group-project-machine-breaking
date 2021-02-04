7ab79be019195d0ca00f7685ad263e77a93a85b6
Nina Kahr <nina.kahr20@imperial.ac.uk>
2021-02-04 16:55:48 +0100

    Rename main file & ask user whether tests should be run

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "tests.h"
+
+int main()
+{
+    std::string answer;
+    std::cout << "Would you like to run tests? Type 'yes' or 'no' and hit enter." << std::endl;
+    std::cout << ">> ";
+    std::cin >> answer;
+
+    if (answer == "yes")
+    {
+        run_tests();
+    }
+    else
+    {
+        std::cout << "Ok, bye." << std::endl;
+    }
+}
